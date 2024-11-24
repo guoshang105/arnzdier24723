2fd011da381cd58725e2a46ded1a0b715ba6e9cd
guoshang105 <115130553+guoshang105@users.noreply.github.com>
2024-11-24 13:11:57 +0800

    Add files via upload

--- template/backup.sh
+++ template/backup.sh
@@ -3,7 +3,6 @@
 # backup.sh 传参 a 自动还原； 传参 m 手动还原； 传参 f 强制更新面板 app 文件及 cloudflared 文件，并备份数据至成备份库。
 # 如是 IPv6 only 或者大陆机器，需要 Github 加速网，可自行查找放在 GH_PROXY 处 ，如 https://mirror.ghproxy.com/ ，能不用就不用，减少因加速网导致的故障。
 
-GH_PROXY=
 GH_PAT=
 GH_BACKUP_USER=
 GH_EMAIL=
@@ -63,10 +62,10 @@ touch $(awk -F '=' '/NO_ACTION_FLAG/{print $2; exit}' $WORK_DIR/restore.sh)1
 [ "$1" = 'f' ] && WAY=Manualed && FORCE_UPDATE=true
 
 # 检查更新面板主程序 app 及 cloudflared
-cd $WORK_DIR
-DASHBOARD_NOW=$(./app -v)
-DASHBOARD_LATEST=$(wget -qO- "https://api.github.com/repos/naiba/nezha/releases/latest" | awk -F '"' '/"tag_name"/{print $4}')
-[[ "$DASHBOARD_LATEST" =~ ^v([0-9]{1,3}\.){2}[0-9]{1,3}$ && "$DASHBOARD_NOW" != "$DASHBOARD_LATEST" ]] && DASHBOARD_UPDATE=true
+# cd $WORK_DIR
+# DASHBOARD_NOW=$(./app -v)
+# DASHBOARD_LATEST=$(wget -qO- "https://api.github.com/repos/naiba/nezha/releases/latest" | awk -F '"' '/"tag_name"/{print $4}')
+# [[ "$DASHBOARD_LATEST" =~ ^v([0-9]{1,3}\.){2}[0-9]{1,3}$ && "$DASHBOARD_NOW" != "$DASHBOARD_LATEST" ]] && DASHBOARD_UPDATE=true
 
 CLOUDFLARED_NOW=$(./cloudflared -v | awk '{for (i=0; i<NF; i++) if ($i=="version") {print $(i+1)}}')
 CLOUDFLARED_LATEST=$(wget -qO- https://api.github.com/repos/cloudflare/cloudflared/releases/latest | awk -F '"' '/tag_name/{print $4}')
@@ -85,33 +84,12 @@ if [[ -n "$GH_REPO" && -n "$GH_BACKUP_USER" && -n "$GH_EMAIL" && -n "$GH_PAT" ]]
 fi
 
 # 分步骤处理
-if [[ "${DASHBOARD_UPDATE}${CLOUDFLARED_UPDATE}${IS_BACKUP}${FORCE_UPDATE}" =~ true ]]; then
-  # 更新面板和 resource
-  if [[ "${DASHBOARD_UPDATE}${FORCE_UPDATE}" =~ 'true' ]]; then
-    hint "\n Renew dashboard app to $DASHBOARD_LATEST \n"
-    wget -O /tmp/dashboard.zip ${GH_PROXY}https://github.com/naiba/nezha/releases/download/$DASHBOARD_LATEST/dashboard-linux-$ARCH.zip
-    unzip /tmp/dashboard.zip -d /tmp
-    if [ -s /tmp/dist/dashboard-linux-$ARCH ]; then
-      info "\n Restart Nezha Dashboard \n"
-      if [ "$IS_DOCKER" = 1 ]; then
-        supervisorctl stop nezha >/dev/null 2>&1
-        sleep 10
-        mv -f /tmp/dist/dashboard-linux-$ARCH $WORK_DIR/app
-        supervisorctl start nezha >/dev/null 2>&1
-      else
-        cmd_systemctl disable >/dev/null 2>&1
-        sleep 10
-        mv -f /tmp/dist/dashboard-linux-$ARCH $WORK_DIR/app
-        cmd_systemctl enable >/dev/null 2>&1
-      fi
-    fi
-    rm -rf /tmp/dist /tmp/dashboard.zip
-  fi
+
 
   # 更新 cloudflared
   if [[ "${CLOUDFLARED_UPDATE}${FORCE_UPDATE}" =~ 'true' ]]; then
     hint "\n Renew Cloudflared to $CLOUDFLARED_LATEST \n"
-    wget -O /tmp/cloudflared ${GH_PROXY}https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-$ARCH && chmod +x /tmp/cloudflared
+    wget -O /tmp/cloudflared https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-$ARCH && chmod +x /tmp/cloudflared
     if [ -s /tmp/cloudflared ]; then
       info "\n Restart Argo \n"
       if [ "$IS_DOCKER" = 1 ]; then
